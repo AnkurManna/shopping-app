70993f44906db0276d03ff50c38942758d5a9c00
Ankur Manna <ankurma@zeta.tech>
2023-07-18 01:17:30 +0530

    [adv]: added shell-script to run zipkin and redis in docker

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ orderservice="./orderservice/"
 paymentservice="./paymentservice/"
 
 # Function to navigate to a directory, build, and run the Spring Boot project in the background
-run_service() {
+run_core_service() {
     local project_dir=$1
     local service=$2
 
@@ -25,11 +25,20 @@ run_service() {
     cd ..
 }
 
+run_utility_service()
+{
+  echo "Running Utility Services "
+  docker run --name localzipkin -d -p 9411:9411 openzipkin/zipkin
+  echo "Started zipkin container with name localzipkin"
+  docker run --name localredis -d -p 6379:6379 redis
+  echo "Started redis container with name localredis"
+}
 # Run the Spring Boot projects
-run_service "$serviceregistry" "serviceregistry"
-run_service "$configserver" "configserver"
-run_service "$cloudgateway" "cloudgateway"
-run_service "$productservice" "productservice"
-run_service "$orderservice" "orderservice"
-run_service "$paymentservice" "paymentservice"
+run_utility_service
+run_core_service "$serviceregistry" "serviceregistry"
+run_core_service "$configserver" "configserver"
+run_core_service "$cloudgateway" "cloudgateway"
+run_core_service "$productservice" "productservice"
+run_core_service "$orderservice" "orderservice"
+run_core_service "$paymentservice" "paymentservice"
 #to do : develop script which doesn't discard logs i.e. opens a new terminal and run process in that
